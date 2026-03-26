ff0b31e51b8aaa35490130018ecb2a0f3e14972b
Uema Yohei <uema.yohei@outlook.jp>
2026-03-26 15:22:02 +0900

    refactor: extract matcher service (#45)
    * refactor: extract matcher service
    
    * fix: remove matcher dependency on yaml loader

--- src/SemanticStub.Api/Program.cs
+++ src/SemanticStub.Api/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.Configure<StubSettings>(builder.Configuration.GetSection("StubSettings"));
 builder.Services.AddSingleton<StubDefinitionLoader>();
+builder.Services.AddSingleton<MatcherService>();
 builder.Services.AddSingleton<StubService>();
 
 var app = builder.Build();
