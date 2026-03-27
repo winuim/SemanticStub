6b89fa7003a5313d5b18a336c917127932d94dd7
Uema Yohei <16240839+winuim@users.noreply.github.com>
2026-03-27 09:16:36 +0900

    feat: add x-delay support (#57)
    * fix: add x-delay support
    
    * test: tighten x-delay integration assertion

--- src/SemanticStub.Api/Models/QueryMatchResponseDefinition.cs
+++ src/SemanticStub.Api/Models/QueryMatchResponseDefinition.cs
@@ -6,6 +6,9 @@ public sealed class QueryMatchResponseDefinition
 {
     public int StatusCode { get; init; }
 
+    [YamlMember(Alias = "x-delay", ApplyNamingConventions = false)]
+    public int? DelayMilliseconds { get; init; }
+
     [YamlMember(Alias = "x-response-file", ApplyNamingConventions = false)]
     public string? ResponseFile { get; init; }
 
--- src/SemanticStub.Api/Models/StubResponse.cs
+++ src/SemanticStub.Api/Models/StubResponse.cs
@@ -6,6 +6,8 @@ public sealed class StubResponse
 {
     public int StatusCode { get; init; }
 
+    public int? DelayMilliseconds { get; init; }
+
     public string ContentType { get; init; } = "application/json";
 
     public IReadOnlyDictionary<string, StringValues> Headers { get; init; } = new Dictionary<string, StringValues>(StringComparer.OrdinalIgnoreCase);
