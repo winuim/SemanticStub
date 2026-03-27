2cabeef39aad24d0e439b0e436b09f58195e9d39
Uema Yohei <uema.yohei@outlook.jp>
2026-03-27 18:11:28 +0900

    feat: support partial query matching

--- src/SemanticStub.Api/Models/QueryMatchDefinition.cs
+++ src/SemanticStub.Api/Models/QueryMatchDefinition.cs
@@ -6,6 +6,9 @@ public sealed class QueryMatchDefinition
 {
     public Dictionary<string, object?> Query { get; init; } = new(StringComparer.Ordinal);
 
+    [YamlMember(Alias = "x-query-partial", ApplyNamingConventions = false)]
+    public Dictionary<string, object?> PartialQuery { get; init; } = new(StringComparer.Ordinal);
+
     public Dictionary<string, string> Headers { get; init; } = new(StringComparer.OrdinalIgnoreCase);
 
     public object? Body { get; init; }
