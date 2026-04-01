7e4054cc0623be3ee0f2a244cdc92ef29ca0a39a
Uema Yohei <uema.yohei@outlook.jp>
2026-04-01 22:37:57 +0900

    feat(api): add HTTP logging middleware (#79)
    * feat(api): add HTTP logging middleware
    
    * fix(api): address HTTP logging review feedback

--- src/SemanticStub.Api/Program.cs
+++ src/SemanticStub.Api/Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.AspNetCore.HttpLogging;
 using SemanticStub.Api.Infrastructure.Yaml;
 using SemanticStub.Api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddHttpLogging(options =>
+{
+    ConfigureHttpLoggingDefaults(options);
+    builder.Configuration.GetSection("HttpLogging").Bind(options);
+    ConfigureHttpLoggingMediaTypes(options);
+});
 builder.Services.Configure<StubSettings>(builder.Configuration.GetSection("StubSettings"));
 builder.Services.AddSingleton<StubDefinitionLoader>();
 builder.Services.AddSingleton<IStubDefinitionLoader>(serviceProvider => serviceProvider.GetRequiredService<StubDefinitionLoader>());
@@ -21,8 +28,25 @@ var app = builder.Build();
 // Fail fast during startup when stub definitions are invalid instead of deferring configuration errors until the first request.
 app.Services.GetRequiredService<StubDefinitionState>();
 
+app.UseHttpLogging();
 app.MapControllers();
 
 app.Run();
+
+static void ConfigureHttpLoggingDefaults(HttpLoggingOptions options)
+{
+    options.LoggingFields =
+        HttpLoggingFields.RequestPropertiesAndHeaders |
+        HttpLoggingFields.ResponsePropertiesAndHeaders |
+        HttpLoggingFields.RequestBody |
+        HttpLoggingFields.ResponseBody;
+}
+
+static void ConfigureHttpLoggingMediaTypes(HttpLoggingOptions options)
+{
+    options.MediaTypeOptions.AddText("text/*");
+    options.MediaTypeOptions.AddText("application/json");
+    options.MediaTypeOptions.AddText("application/xml");
+}
 
 public partial class Program;
